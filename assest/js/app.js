1197af8aeda2e226ed8b4d7b2936f2d3e0d6c219
md solayman mollik <mdsolayman7814@gmail.com>
2024-12-14 18:08:50 +0600

    Update app.js

--- assest/js/app.js
+++ assest/js/app.js
@@ -36,3 +36,30 @@ bar.addEventListener("click", () => {
     bar.classList.remove("fa-xmark");
   }
 });
+
+
+
+
+
+
+
+
+function MakeFunc(name, age, country, language, stall, sell, buy, profit) {
+  this.name = name;
+  this.id = age;
+  this.country = country;
+  this.language = language;
+  this.stall = stall;
+  this.sell = sell;
+  this.buy = buy;
+  this.profit = profit;
+}
+
+let productList = [];
+
+for (let i = 2; i <= 500; i++) {
+  let make = new MakeFunc("solayman", i, "Bangladesh", "Bangla");
+  productList.push(make);
+}
+
+console.table(productList);
